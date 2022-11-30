7e268bd18647ee8083c92d560bd9939999321a11
Husan-Negmatullaev <husannegmatullaev21@gmail.com>
2022-11-30 15:12:41 +0500

    'add-many-pages'

--- src/components/tables/HeadingTables/HeadingTablesRow.jsx
+++ src/components/tables/HeadingTables/HeadingTablesRow.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import clsx from "clsx";
-
-import styles from "./HeadingTables.module.scss";
+// import clsx from "clsx";
+//
+// import styles from "./HeadingTables.module.scss";
 
 const HeadingTablesRow = () => {
 
